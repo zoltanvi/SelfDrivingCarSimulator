b120b1117887a7b15d52175af45c2de05187b20b
zoltanvi <viczezoltan@gmail.com>
2018-02-19 14:26:02 +0100

    Created checkpoints and a trigger
    Next: be able to measure distance with the checkpoints

---
+++ Assets/Scripts/FitnessMeter.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FitnessMeter : MonoBehaviour
+{
+
+	#region Variables
+	#endregion
+
+	#region Unity Methods
+
+	void OnTriggerEnter(Collider other)
+	{
+		if (other.gameObject.layer == LayerMask.NameToLayer("Checkpoints"))
+		{
+			Debug.Log(other.gameObject.name);
+		}
+		else
+		{
+			Debug.Log("wall... i think");
+		}
+	}
+
+	void Update()
+	{
+
+	}
+
+	#endregion
+
+}
--- Assets/Scripts/WallSensor.cs
+++ Assets/Scripts/WallSensor.cs
@@ -1,12 +1,13 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public class WallSensor : MonoBehaviour {
+public class WallSensor : MonoBehaviour
+{
 
 	[SerializeField] private Transform m_RayOrigin;
 	[SerializeField] private float m_lineDistance = 4f;
 	[SerializeField] private int m_rayQuantity = 2;
-	[SerializeField] private string m_sensorTag = "Wall";
+	[SerializeField] private string m_rayLayerName = "Environment";
 	[SerializeField] private Text m_sensorText;
 	private string m_raycastText;
 
@@ -14,75 +15,54 @@ public class WallSensor : MonoBehaviour {
 	private RaycastHit[] raycastHit;
 
 	void Start()
-     {
-        raycastHit = new RaycastHit[m_rayQuantity + 1];
+	{
+		raycastHit = new RaycastHit[m_rayQuantity + 1];
 		m_raycastText = "";
-     }
+	}
 
 
-     // Update is called once per frame
-     void FixedUpdate()
-     {
+	// Update is called once per frame
+	void FixedUpdate()
+	{
 		CreateRays(m_rayQuantity);
 
 		m_raycastText = "";
 		for (int i = 0; i < raycastHit.Length; i++)
 		{
-			m_raycastText += (i+1) + ". sensor: " + raycastHit[i].distance.ToString() + "\n";
+			m_raycastText += (i + 1) + ". sensor: " + raycastHit[i].distance.ToString() + "\n";
 			//m_raycastText += raycastHit[i].distance.ToString() + "\n";
 		}
 		m_sensorText.text = m_raycastText;
-     }
+	}
 
-     // Mindket oldalra quantity-1 db sugarat fog vetni, + kozepre egyet.
-     void CreateRays(int quantity)
-     {
-          // angleBase = a sugarak kozotti szog nagysaga
-          float angleBase = 90f / quantity;
-          for (int i = 1, j = (quantity - 1); i < (quantity); i++, j--)
-          {
-               // A szogek jobb es bal oldalra novekednek / csokkennek, angleBase-enkent
-               Quaternion rightRot = Quaternion.AngleAxis(angleBase * i, m_RayOrigin.up);
-               Quaternion leftRot = Quaternion.AngleAxis(-angleBase * i, m_RayOrigin.up);
+	// Mindket oldalra quantity-1 db sugarat fog vetni, + kozepre egyet.
+	void CreateRays(int quantity)
+	{
+		// angleBase = a sugarak kozotti szog nagysaga
+		float angleBase = 90f / quantity;
+		for (int i = 1, j = (quantity - 1); i < (quantity); i++, j--)
+		{
+			// A szogek jobb es bal oldalra novekednek / csokkennek, angleBase-enkent
+			Quaternion rightRot = Quaternion.AngleAxis(angleBase * i, m_RayOrigin.up);
+			Quaternion leftRot = Quaternion.AngleAxis(-angleBase * i, m_RayOrigin.up);
 
-               // Megrajzolja a bal es jobb oldali sugarakat
-               Debug.DrawRay(m_RayOrigin.position, rightRot * m_RayOrigin.forward * m_lineDistance, Color.blue);
-               Debug.DrawRay(m_RayOrigin.position, leftRot * m_RayOrigin.forward * m_lineDistance, Color.red);
+			// Megrajzolja a bal es jobb oldali sugarakat
+			Debug.DrawRay(m_RayOrigin.position, rightRot * m_RayOrigin.forward * m_lineDistance, Color.yellow);
+			Debug.DrawRay(m_RayOrigin.position, leftRot * m_RayOrigin.forward * m_lineDistance, Color.yellow);
 
-               //A jobb oldali sugar
-               if (Physics.Raycast(m_RayOrigin.position, rightRot * m_RayOrigin.forward, out raycastHit[i - 1], m_lineDistance))
-               {
-                    if (raycastHit[i-1].collider.tag == m_sensorTag)
-                    {
-                       //  Debug.Log(i + ". sensor: " + raycastHit[i-1].distance);
-                    }
-               }
+			// Letrehozza a bal es jobb oldali sugarakat
+			Physics.Raycast(m_RayOrigin.position, rightRot * m_RayOrigin.forward, out raycastHit[i - 1], m_lineDistance, LayerMask.GetMask(m_rayLayerName));
+			Physics.Raycast(m_RayOrigin.position, leftRot * m_RayOrigin.forward, out raycastHit[j], m_lineDistance, LayerMask.GetMask(m_rayLayerName));
+			
+		}
 
-               // A bal oldali sugar
-               if (Physics.Raycast(m_RayOrigin.position, leftRot * m_RayOrigin.forward, out raycastHit[j], m_lineDistance))
-               {
-                    if (raycastHit[j].collider.tag == m_sensorTag)
-                    {
-                         //Debug.Log((j+1) + ". sensor: " + raycastHit[j].distance);
-                    }
-               }
-          }
+		// Megrajzolja a kozepso sugarat
+		Debug.DrawRay(m_RayOrigin.position, m_RayOrigin.forward * m_lineDistance, Color.yellow);
+		// Letrehozza a kozepso sugarat
+		Physics.Raycast(m_RayOrigin.position, m_RayOrigin.forward, out raycastHit[quantity], m_lineDistance, LayerMask.GetMask(m_rayLayerName));
+	
 
-          // Megrajzolja a kozepso sugarat
-          Debug.DrawRay(m_RayOrigin.position, m_RayOrigin.forward * m_lineDistance, Color.green);
-
-          // A kozepso sugar 
-          if (Physics.Raycast(m_RayOrigin.position, m_RayOrigin.forward, out raycastHit[quantity], m_lineDistance))
-          {
-               if (raycastHit[quantity].collider.tag == m_sensorTag)
-               {
-                //    Debug.Log((quantity + 1) + ". sensor: " + raycastHit[quantity].distance);
-               }
-          }
-
-
-
-     }
+	}
 
 
 }
