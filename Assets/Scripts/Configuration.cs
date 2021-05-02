060ba2f5ad5bfb014bc905fcb8e62f32272fb180
Zoltan <viczezoltan@gmail.com>
2021-05-02 16:13:24 +0200

    Added license information to src

--- Assets/Scripts/Configuration.cs
+++ Assets/Scripts/Configuration.cs
@@ -1,4 +1,21 @@
-﻿[System.Serializable]
+﻿/*
+Copyright (C) 2021 zoltanvi
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+[System.Serializable]
 public class Configuration
 {
     public int CarCount { get; set; }
